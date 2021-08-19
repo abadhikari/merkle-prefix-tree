125b71906dc923918e59ef1dcbc40b292457ac63
Abhinna Adhikari <adhikari.abhinna@gmail.com>
2021-08-19 02:47:17 -0700

    Add better error handling, make instance variables private, add getters

--- merkle_prefix_tree.py
+++ merkle_prefix_tree.py
@@ -65,19 +65,19 @@ class MerklePrefixTree:
         Raises:
             ValueError: If height of tree is less than 1.
         """
-        self.height = height
+        self._height = height
 
         # Verify that height of tree is greater than 0
         if height < 1:
             error_msg = 'invalid height of tree. Height must be greater than 0'
             raise ValueError(error_msg)
 
-        self.append_only = append_only
-        self.hash_func = hash_func if hash_func else self.default_hash_func
-        self.serialize_func = serialize_func if serialize_func else self.default_serialize_func
+        self._append_only = append_only
+        self._hash_func = hash_func if hash_func else self.default_hash_func
+        self._serialize_func = serialize_func if serialize_func else self.default_serialize_func
 
-        self.root_node = InteriorNode()
-        self.empty_tree_hash_dict = self._precompute_empty_hashes()
+        self._root_node = InteriorNode()
+        self._empty_tree_hash_dict = self._precompute_empty_hashes()
 
     def _precompute_empty_hashes(self):
         """Precompute hashes of theoretical empty nodes at each level in tree.
@@ -89,19 +89,19 @@ class MerklePrefixTree:
         """
         # k_empty is used in the calculation of an EmptyNode
         k_empty = bin(0)
-        curr_hash = self.hash_func(k_empty)
+        curr_hash = self._hash_func(k_empty)
 
         # Init hash_dict with the leaf EmptyNode
-        hash_dict = {self.height: curr_hash}
-        for i in range(self.height):
-            curr_hash = self.hash_func(curr_hash + curr_hash)
+        hash_dict = {self._height: curr_hash}
+        for i in range(self._height):
+            curr_hash = self._hash_func(curr_hash + curr_hash)
 
             # Traversing the tree down up when calculating these hashes
-            curr_height = self.height - i - 1
+            curr_height = self._height - i - 1
             hash_dict[curr_height] = curr_hash
 
         # When the tree is empty, the root will have an empty hash
-        self.root_node.hash = curr_hash
+        self._root_node.hash = curr_hash
         return hash_dict
 
     def get_root_hash(self):
@@ -110,7 +110,7 @@ class MerklePrefixTree:
         Returns:
             A bytes object.
         """
-        return self.root_node.hash
+        return self._root_node.hash
 
     def get_data_node(self, prefix):
         """Return the DataNode at the given prefix within the tree.
@@ -124,15 +124,13 @@ class MerklePrefixTree:
 
         Raises:
             ValueError: If length of prefix is not equivalent to height of
-                the tree.
+                the tree or prefix contains characters not in the set
+                {'0', '1'}.
         """
-        if len(prefix) != self.height:
-            height = str(self.height)
-            error_msg = 'invalid prefix length. Must be length of ' + height
-            raise ValueError(error_msg)
+        self._verify_prefix(prefix)
 
         # Traverse the tree following the prefix bit by bit
-        curr_node = self.root_node
+        curr_node = self._root_node
         for bit in prefix:
             curr_node = curr_node.left if bit == '0' else curr_node.right
 
@@ -148,15 +146,25 @@ class MerklePrefixTree:
             prefix: A string of 0's and 1's that maps to a leaf in the tree.
             to_append: The object that will be appended to the tree at the
             given prefix.
+
+        Raises:
+            ValueError: If length of prefix is not equivalent to height of
+                the tree or prefix contains characters not in the set
+                {'0', '1'}.
+            AppendOnlyError: If trying to append to existing prefix when
+                append_only instance variable is set to True.
         """
+        self._verify_prefix(prefix)
+
+        # Create the new DataNode that will be appended
         new_data_node = DataNode(prefix[-1],
                                  to_append,
-                                 self.hash_func,
-                                 self.serialize_func)
+                                 self._hash_func,
+                                 self._serialize_func)
 
         # Add nodes to the tree
-        curr_node = self.root_node
-        for i in range(self.height):
+        curr_node = self._root_node
+        for i in range(self._height):
 
             # Find the next bit of the prefix on the path within the tree
             bit = prefix[i]
@@ -165,22 +173,20 @@ class MerklePrefixTree:
 
             # Set the parent and the parent's left and right node pointers
             new_node = InteriorNode(bit)
-            if i == self.height - 1:
+            if i == self._height - 1:
                 new_node = new_data_node
             new_node.parent = prev_node
             if bit == '0':
                 new_node.parent.left = new_node
-            elif bit == '1':
-                new_node.parent.right = new_node
             else:
-                raise ValueError('invalid prefix: ' + prefix)
+                new_node.parent.right = new_node
 
             # Add node depending on the current level within the tree
-            if i < self.height - 1:
+            if i < self._height - 1:
                 if not curr_node:
                     curr_node = new_node
             else:
-                if not self.append_only:
+                if not self._append_only:
                     curr_node = new_node
                 else:
                     error_msg = ("cannot append because DataNode"
@@ -189,9 +195,35 @@ class MerklePrefixTree:
                     raise AppendOnlyError(error_msg)
 
         # Update the hashes of the impacted nodes
-        self.rehash(curr_node)
+        self._rehash(curr_node)
+    
+    def _verify_prefix(self, prefix):
+        """Verify if prefix is valid length and contains valid character.
+        
+        A valid prefix will have a length that is equivalent to the height
+        of the tree. A valid prefix will also only have characters in the
+        set {'0', '1'}.
 
-    def rehash(self, leaf_node):
+        Args:
+            prefix: A string of 0's and 1's that maps to a leaf in the tree.
+
+        ValueError: If length of prefix is not equivalent to height of
+            the tree or prefix contains characters not in the set
+            {'0', '1'}.
+        """
+        if len(prefix) != self._height:
+            height = str(self._height)
+            error_msg = 'invalid prefix length. Must be length of ' + height
+            raise ValueError(error_msg)
+        
+        # Iterate through prefix and verify that it only contains valid chars
+        for char in prefix:
+            if char not in ('0', '1'):
+                error_msg = ('invalid prefix: ' + prefix +
+                             '. Permitted characters are in set {0, 1}')
+                raise ValueError(error_msg)
+
+    def _rehash(self, leaf_node):
         """Rehash the hashes of the nodes from leaf_node to root_node.
 
         Starting at the leaf_node, the rehash function will travel
@@ -209,14 +241,16 @@ class MerklePrefixTree:
             raise TypeError('cannot re_hash. leaf_node is not a DataNode')
 
         curr_node = leaf_node
-        for i in range(self.height - 1, -1, -1):
+        for i in range(self._height - 1, -1, -1):
             curr_node = curr_node.parent
-            empty_hash = self.empty_tree_hash_dict[i + 1]
+            empty_hash = self._empty_tree_hash_dict[i + 1]
+
+            # Using left and right node hash, recompute the hash of curr_node
             left_hash = curr_node.left.hash if curr_node.left else empty_hash
             right_hash = curr_node.right.hash if curr_node.right else empty_hash
             curr_node.hash = self.hash_hashes(left_hash,
                                               right_hash,
-                                              self.hash_func)
+                                              self._hash_func)
 
     def produce_inclusion_proof(self, prefix):
         """Produce proof that DataNode at prefix is included within the tree.
@@ -235,10 +269,16 @@ class MerklePrefixTree:
             inclusion_proof: A list of hashes of the nodes along the path from
                 the given DataNode (DataNode hash not included) that
                 corresponds to the prefix to the root_node of the tree.
+
+        ValueError: If length of prefix is not equivalent to height of
+            the tree or prefix contains characters not in the set
+            {'0', '1'}.
         """
-        curr_node = self.root_node
+        self._verify_prefix(prefix)
+
+        curr_node = self._root_node
         inclusion_proof = []
-        for i in range(self.height):
+        for i in range(self._height):
             bit = prefix[i]
             opp_node = curr_node.right if bit == '0' else curr_node.left
             curr_node = curr_node.left if bit == '0' else curr_node.right
@@ -246,7 +286,7 @@ class MerklePrefixTree:
             # Return empty list when data node is not included in tree
             if not curr_node:
                 return []
-            empty_tree_hash = self.empty_tree_hash_dict[i + 1]
+            empty_tree_hash = self._empty_tree_hash_dict[i + 1]
             new_proof_hash = opp_node.hash if opp_node else empty_tree_hash
             inclusion_proof.append(new_proof_hash)
         return inclusion_proof
@@ -278,15 +318,21 @@ class MerklePrefixTree:
         Returns:
             A boolean that confirms if the given proof of inclusion is valid
                 or not.
+
+        ValueError: If length of prefix is not equivalent to height of
+            the tree or prefix contains characters not in the set
+            {'0', '1'}.
         """
+        self._verify_prefix(prefix)
+
         calculated_hash = included_hash
         for i in range(len(inclusion_proof) - 1, -1, -1):
             bit = prefix[i]
             left_hash = calculated_hash if bit == '0' else inclusion_proof[i]
             right_hash = calculated_hash if bit == '1' else inclusion_proof[i]
-            calculated_hash = hash_hashes(left_hash,
+            calculated_hash = self.hash_hashes(left_hash,
                                           right_hash,
-                                          self.hash_func)
+                                          self._hash_func)
         return calculated_hash == root_hash
 
     def pretty_print(self):
@@ -296,7 +342,7 @@ class MerklePrefixTree:
         left and right subtree are clearly seen through the use of '├── '
         for left subtrees and '└── ' for right subtrees.
         """
-        curr_node = self.root_node
+        curr_node = self._root_node
 
         def recurse_tree(root,
                          level=0,
@@ -310,8 +356,8 @@ class MerklePrefixTree:
                 level_divider = prefix_print + '└── '
                 prefix_print += '    '
             if not root:
-                if level <= self.height:
-                    empty_hash = str(self.empty_tree_hash_dict[level])
+                if level <= self._height:
+                    empty_hash = str(self._empty_tree_hash_dict[level])
                     print(level_divider + empty_hash)
                 return
             print(level_divider + str(root.hash))
@@ -322,18 +368,42 @@ class MerklePrefixTree:
     def get_prefix_path_lst(self, prefix):
         """Return a list of nodes on the path corresponding to the prefix.
 
+        Method used for debugging purposes.
+
         Args:
             prefix: A string of 0's and 1's that maps to a leaf in the tree.
 
         Returns:
             path_lst: A list of InteriorNodes and one leaf DataNode.
+
+        ValueError: If length of prefix is not equivalent to height of
+            the tree or prefix contains characters not in the set
+            {'0', '1'}.
         """
-        curr_node = self.root_node
+        self._verify_prefix(prefix)
+
+        curr_node = self._root_node
         path_lst = [curr_node]
         for bit in prefix:
             curr_node = curr_node.left if bit == '0' else curr_node.right
             path_lst.append(curr_node)
         return path_lst
+
+    def get_tree_height(self):
+        """Return the height of the MerklePrefixTree."""
+        return self._height
+
+    def get_hash_func(self):
+        """Return the hash function that is currently used."""
+        return self._hash_func
+
+    def get_serialize_func(self):
+        """Return the serialize function that is currently used."""
+        return self._serialize_func
+
+    def is_append_only(self):
+        """Return if MerklePrefixTree is append_only or not."""
+        return self._append_only
 
     @staticmethod
     def hash_hashes(hash_1,
@@ -395,5 +465,5 @@ class MerklePrefixTree:
             A string of the serialized data.
         """
         if not isinstance(to_serialize, str):
-            return json.dumps(obj, default=lambda obj: obj.__dict__)
+            return json.dumps(to_serialize, default=lambda obj: obj.__dict__)
         return to_serialize
